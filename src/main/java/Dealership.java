ca7efdcebdb08485140aeedd4dc9669e24c18021
fatpav <fat_pav@hotmail.com>
2020-11-25 15:51:23 +0000

    dealer tests

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public class Dealership {
 
+    private ArrayList<Vehicle> stock;
+    private double till;
+
+    public Dealership(){
+        this.till = till;
+        this.stock = new ArrayList<Vehicle>();
+    }
+
+    public int getStock() {
+        return this.stock.size();
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        this.stock.add(vehicle);
+    }
 }
---
+++ src/test/java/DealerTest.java
@@ -0,0 +1,46 @@
+import components.Doors;
+import components.Engine;
+import components.Seats;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DealerTest {
+
+    Vehicle vehicle;
+    Dealership dealership;
+    Engine engine;
+    Doors doors;
+    Seats seats;
+
+    @Before
+    public void setUp(){
+        dealership = new Dealership();
+        engine = new Engine(1.6, 260, "Diesel");
+        doors = new Doors(4);
+        seats = new Seats(5);
+        vehicle = new Car(engine, doors, seats, "diesel", "Ford", "red", 100);
+    }
+
+    @Test
+    public void dealerHasNoCars(){
+        assertEquals(0, dealership.getStock());
+    }
+
+    @Test
+    public void dealerHasNoMoney(){
+        assertEquals(0.00, dealership.getTill(), 0.00);
+    }
+
+    @Test
+    public void dealerCanAddCarToStock(){
+        dealership.addVehicle(vehicle);
+        assertEquals(1, dealership.getStock());
+    }
+
+    @Test
+    publ
+
+
+}
